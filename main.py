2ddb0563de4480d40a4028f6318d28958132c729
Umbrasyl <104720897+Umbrasyl@users.noreply.github.com>
2026-09-04 23:17:05 +0200

    add turning and moving events with key press

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    curr_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        curr_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        curr_player.update(dt)
         curr_player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
